05361bc697b808d768404ddac9f160caed8e727c
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-05-01 08:44:03 +0900

    emacs-lisp-modeのフックを設定

--- init.el
+++ init.el
@@ -27,3 +27,12 @@
 ;; 文字コードを指定
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
