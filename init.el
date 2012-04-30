97470f65c64236e99a4ef729b7267431f3a97e03
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-04-30 18:24:13 +0900

    サブディレクトリをload-pathに追加するように設定

--- init.el
+++ init.el
@@ -13,3 +13,14 @@
 ;; フェイスを変更する
 (set-face-background 'show-paren-match-face nil)
 (set-face-underline-p 'show-paren-match-face "red")
+;; load_path を追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+;; 引数のディレクトリとそのサブディレクトリをload-pathを追加
+(add-to-load-path "elisp" "conf" "public_repos")
