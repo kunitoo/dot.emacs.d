4400c67f881adc23b0c59ed762ee59e616ff243c
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-05-01 15:20:10 +0900

    add package.el

--- init.el
+++ init.el
@@ -2,17 +2,6 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
 ;; "C-t"でウィンドウを切り替える。初期値はtranspose-chars
 (global-set-key (kbd "C-t") 'other-window)
-;; 現在行をハイライト
-(global-hl-line-mode t)
-;; 対応する括弧の強調設定
-;; paren-mode:対応する括弧を強調して表示する
-(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
-(show-paren-mode t) ; 有効化
-;; parenのスタイル: expressionは括弧内も強調表示
-(setq show-paren-style 'expression)
-;; フェイスを変更する
-(set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "red")
 ;; load_path を追加する関数を定義
 (defun add-to-load-path (&rest paths)
   (let (path)
@@ -24,6 +13,25 @@
 	    (normal-top-level-add-subdirs-to-load-path))))))
 ;; 引数のディレクトリとそのサブディレクトリをload-pathを追加
 (add-to-load-path "elisp" "conf" "public_repos")
+;; package.el の設定
+(when (require 'package nil t)
+  ;; パッケージリポジトリにMarmaladeと開発者運営のELPAを追加
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa"))
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
+;; 現在行をハイライト
+(global-hl-line-mode t)
+;; 対応する括弧の強調設定
+;; paren-mode:対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "red")
 ;; 文字コードを指定
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
