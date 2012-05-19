fee13992220bd3ba01dc1a4c5eb4460811de8c7d
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-05-19 15:30:05 +0900

    anything-show-kill-ringをM-yに割り当てた

--- inits/99_global-key-binds.el
+++ inits/99_global-key-binds.el
@@ -2,3 +2,5 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
 ;; "C-t"でウィンドウを切り替える。初期値はtranspose-chars
 (global-set-key (kbd "C-t") 'other-window)
+;; M-yにanything-show-kill-ringを割り当てる
+(global-set-key (kbd "M-y") 'anything-show-kill-ring)
