6934606b1394f353b72a1dedb9f4aefd5a093e30
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-05-19 16:25:58 +0900

    add auto-complete

---
+++ inits/50_auto_complete.el
@@ -0,0 +1,7 @@
+;; M-x package-install RET auto-complete RET でインストール
+;; auto-completeの設定
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories
+	       "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
